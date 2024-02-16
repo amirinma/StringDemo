eede10259e89aa743331ebb80fb86bd4346f9afc
ziaro <zia.ro@live.com>
2024-02-15 19:43:14 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+       String greeting = "Good morning, ";
+       String name = "Herman Zia";
+
+       System.out.println(greeting + name);
+    }
+}
